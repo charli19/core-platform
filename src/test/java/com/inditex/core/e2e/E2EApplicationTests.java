2bae928064b8bc687d0f4cd99abed1bad3ceaa5d
Carles Saumell <saumellcarles@gmail.com>
2025-09-03 16:50:11 +0200

    restore karate test config

--- src/test/java/com/inditex/core/e2e/E2EApplicationTests.java
+++ src/test/java/com/inditex/core/e2e/E2EApplicationTests.java
@@ -1,17 +1,8 @@
 package com.inditex.core.e2e;
 
-import com.intuit.karate.Results;
-import com.intuit.karate.Runner;
 import com.intuit.karate.junit5.Karate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.stream.Stream;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class E2EApplicationTests {
@@ -20,37 +11,10 @@ class E2EApplicationTests {
     private int port;
 
     @Karate.Test
-    Karate testAll() throws IOException {
-        Results results = Runner.path("classpath:e2e/features")
-                .systemProperty("baseUrl", "http://localhost:" + port)
-                .outputCucumberJson(true)
-                .parallel(1);
-
-        copyReports(results.getReportDir());
-
+    Karate testAll() {
         return Karate.run("classpath:e2e/features")
                 .systemProperty("baseUrl", "http://localhost:" + port)
                 .relativeTo(getClass());
     }
-
-    private void copyReports(String sourceDirPath) throws IOException {
-        Path sourceDir = Paths.get(sourceDirPath);
-        Path targetDir = Paths.get("src/main/resources/static/karate/reports");
-        Files.createDirectories(targetDir);
-
-        try (Stream<Path> paths = Files.walk(sourceDir)) {
-            paths.filter(Files::isRegularFile)
-                    .forEach(file -> {
-                        try {
-                            Files.copy(file, targetDir.resolve(file.getFileName()), StandardCopyOption.REPLACE_EXISTING);
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
-        }
-
-        System.out.println("Reporte disponible en: https://core-platform.up.railway.app/karate/reports/karate-summary.html");
-    }
-
 
 }
