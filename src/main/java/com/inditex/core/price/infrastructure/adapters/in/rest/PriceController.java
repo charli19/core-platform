9fae8939b3064265c27a3551e225775957c7cfa1
Carles Saumell <saumellcarles@gmail.com>
2025-09-04 11:33:53 +0200

    update openapi spec and fix test for controller

--- src/main/java/com/inditex/core/price/infrastructure/adapters/in/rest/PriceController.java
+++ src/main/java/com/inditex/core/price/infrastructure/adapters/in/rest/PriceController.java
@@ -1,19 +1,17 @@
 package com.inditex.core.price.infrastructure.adapters.in.rest;
 
 
-import com.inditex.core.infrastructure.adapters.in.rest.spec.PriceApi;
+import com.inditex.core.infrastructure.adapters.in.rest.spec.PricesApi;
 import com.inditex.core.infrastructure.adapters.in.rest.dto.spec.PriceDto;
 import com.inditex.core.price.application.ports.in.GetPricePort;
 import com.inditex.core.price.domain.model.Price;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
 
 @RestController
-@RequestMapping("/api/v1/prices")
-public class PriceController implements PriceApi {
+public class PriceController implements PricesApi {
 
     private final GetPricePort getPricePort;
 
@@ -22,7 +20,7 @@ public class PriceController implements PriceApi {
     }
 
     @Override
-    public ResponseEntity<PriceDto> priceGet(Long brandId, Long productId, OffsetDateTime applicationDate) {
+    public ResponseEntity<PriceDto> pricesGet(Long brandId, Long productId, OffsetDateTime applicationDate) {
         final Price price = getPricePort.getPriorityPrice(brandId, productId, applicationDate);
 
         PriceDto priceDto = new PriceDto();
